81b9f353173b3c0e131a65a32761b36991729a3f
Biatov.roman <cumshoot9@gmail.com>
2023-05-29 22:26:29 +0300

    Second commit

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,55 @@
 <template>
-  <div>
-    <h1>ToDo List</h1>
-    <input type="text" v-model="input" @keypress.enter="addNewTask" />
-    <div>
-      <div class="task-wrapper" v-for="(task, id) in tasks" :key="id">
-        <input type="checkbox" />
-        <div>{{ task.title }}</div>
-        <div class="delete-task" @click="deleteTask(id)">&times;</div>
-      </div>
-    </div>
-  </div>
+  <h1>ToDo List</h1>
+  <input type="text" v-model="input" @keypress.enter="addNewTask" />
+  <Task
+    v-for="task in tasks"
+    :key="task.id"
+    :task="task"
+    @task-completed="completedTask"
+    @task-deleted="deleteTask"
+  />
 </template>
 
 <script>
+import Task from "./components/Task.vue";
+
 export default {
+  components: { Task },
   data() {
     return {
-      tasks: [{ title: "awdawd" }],
+      tasks: [
+        { id: 1, title: "awdawd", completed: false },
+        { id: 2, title: "taras", completed: true },
+        { id: 3, title: "privet", completed: false },
+        { id: 4, title: "kak dela", completed: false },
+      ],
       input: "",
     };
   },
   methods: {
     deleteTask(id) {
-      this.tasks.splice(id, 1);
+      const index = this.tasks.findIndex((task) => task.id === id);
+      if (index !== -1) {
+        this.tasks.splice(index, 1);
+      }
     },
     addNewTask() {
       if (this.input && this.input.trim()) {
         this.tasks.push({
+          id: Date.now(),
           title: this.input,
+          completed: false,
         });
         this.input = "";
       }
+    },
+    completedTask(id) {
+      this.tasks = this.tasks.map((task) => {
+        if (task.id === id) {
+          task.completed = !task.completed;
+        }
+        return task;
+      });
     },
   },
 };
---
+++ src/components/Task.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="task-wrapper">
+    <input
+      type="checkbox"
+      @change="completedTask(task.id)"
+      :checked="task.completed"
+    />
+    <div :style="{ textDecoration: task.completed ? 'line-through' : 'none' }">
+      {{ task.title }}
+    </div>
+    <div class="delete-task" @click="deleteTask(task.id)">&times;</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+  emits: ["task-completed", "task-deleted"],
+  methods: {
+    deleteTask() {
+      this.$emit("task-deleted", this.task.id);
+    },
+  },
+};
+</script>
+
+<style></style>
