adbfdaf05943faf0a6c03064e8efbf21d97782f8
Biatov.roman <cumshoot9@gmail.com>
2023-06-01 00:53:24 +0300

    Create new component AppCheckbox

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import "./style.css";
 import App from "./App.vue";
 import AppIcon from "./components/AppIcon.vue";
 import AppButton from "./components/AppButton.vue";
+import AppCheckbox from "./components/AppCheckbox.vue";
 
 createApp(App)
   .component("AppIcon", AppIcon)
   .component("AppButton", AppButton)
+  .component("AppCheckbox", AppCheckbox)
   .mount("#app");
